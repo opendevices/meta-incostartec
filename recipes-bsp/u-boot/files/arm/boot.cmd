4d898d60c5d93309c29dcf71b4c1b53594e8b67d
Matthias Schmitz <matthias@opendevices.io>
2020-04-29 23:33:44 +0200

    Increase the layer priority / move file to right place
    We need to increase the layer priority to a higher value then
    meta-sunxi. Also the file must be placed in the same folder as the file
    to overwrite.

--- recipes-bsp/u-boot/files/arm/boot.cmd
+++ recipes-bsp/u-boot/files/arm/boot.cmd
@@ -6,8 +6,8 @@ if itest.b *0x28 == 0x02 ; then
 	echo "U-boot loaded from eMMC or secondary SD"
 	rootdev=mmcblk1p2
         setenv bootargs console=${console} console=tty1 root=/dev/${rootdev} rootwait panic=10 ${extra}
-        load mmc 1:1 ${fdt_addr_r} ${fdtfile} 
-        load mmc 1:1 ${kernel_addr_r} zImage || load mmc 1:1 ${kernel_addr_r} uImage 
+        load mmc 1:1 ${fdt_addr_r} ${fdtfile}
+        load mmc 1:1 ${kernel_addr_r} zImage || load mmc 1:1 ${kernel_addr_r} uImage
         bootz ${kernel_addr_r} - ${fdt_addr_r} || bootm ${kernel_addr_r} - ${fdt_addr_r}
 fi
 setenv bootargs console=${console} console=tty1 root=/dev/${rootdev} rootwait panic=10 ${extra}
--- recipes-bsp/u-boot/u-boot_2018.09.bbappend
+++ recipes-bsp/u-boot/u-boot_2018.09.bbappend
@@ -1,7 +1,7 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
 
 SRC_URI_append_plilly-a20 = "file://0001-Add-dts-and-defconfig-for-INCOstartec-pLILLY-A20.patch \
-                             file://boot.cmd"
+                             file://arm/boot.cmd"
 SRC_URI_append_plilly-a20-tbt-x-056t-d = "file://0001-Add-dts-and-defconfig-for-INCOstartec-pLILLY-A20.patch \
                                           file://0002-Add-defconfig-for-INCOstartec-tbt-x-056t-with-pLILLY.patch \
-                                          file://boot.cmd"
+                                          file://arm/boot.cmd"
